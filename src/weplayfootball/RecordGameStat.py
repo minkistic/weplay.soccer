833ff96366767390c167fabf5eff439570b6f8f0
minkistic <59783289+minkistic@users.noreply.github.com>
2020-03-04 21:45:03 +0900

    이벤트타입 별 기록입력 필드 표시하기
    이벤트타입 별 기록입력 필드 표시하기

--- src/weplayfootball/RecordGameStat.py
+++ src/weplayfootball/RecordGameStat.py
@@ -11,7 +11,7 @@ from pymongo import MongoClient  # pymongo를 임포트 하기
 
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbgamestat  # 'dbsparta'라는 이름의 db를 만듭니다.
-#  
+#
 client.dbplayerlist
 
 app = Flask(__name__)
